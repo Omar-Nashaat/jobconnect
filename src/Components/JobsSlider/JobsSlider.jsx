97075c1f4fa436b1245b63ef2aff5105f51a2cd6
Omar-Nashaat <omarnashaat18@gmail.com>
2024-04-04 00:51:03 +0200

    Adding some fake data to jobs

--- src/Components/JobsSlider/JobsSlider.jsx
+++ src/Components/JobsSlider/JobsSlider.jsx
@@ -55,11 +55,29 @@ export default function JobsSlider() {
                     <div class="service-icon d-flex justify-content-center align-items-center m-auto">
                         <i class="fa-solid fa-user-plus fs-4"></i>
                     </div>
-                    <h4 class="pt-4 pb-1">Junior Web Developer</h4>
+                    <h4 class="pt-4 pb-1">Junior Frontend Developer</h4>
                     <p>Egypt , Maddi<i class={style.checkIcon + " fa-solid fa-circle-check ps-2"}></i></p>
                     <div className="mb-3">
-                        <button className={style.jobBtnBlack + " btn rounded-3 me-1"}>Ghana</button>
-                        <button className={style.jobBtnBlack + " btn rounded-3 me-1"}>$ 1000 - 1,500</button>
+                        <button className={style.jobBtnBlack + " btn rounded-3 me-1"}>OnSite</button>
+                        <button className={style.jobBtnBlack + " btn rounded-3 me-1"}>$ 2000 - 3,500</button>
+                        <button className={style.jobBtnBlack + " btn rounded-3 me-1 mb-1"}>With Perks</button>
+                        <button className={style.jobBtnBlack + " btn rounded-3 me-1"}>Full time</button>
+                    </div>
+                    <div>
+                        <button className={style.jobBtnApply + " btn rounded-3 me-1 rounded-3 fs-5"}>Apply<i class="fa-solid fa-link ps-1"></i></button>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class={style.jobCard + " text-center bg-white rounded-3 p-4 shadow-sm m-1"}>
+                    <div class="service-icon d-flex justify-content-center align-items-center m-auto">
+                        <i class="fa-solid fa-user-plus fs-4"></i>
+                    </div>
+                    <h4 class="pt-4 pb-1">UI/UX Developer</h4>
+                    <p>Egypt , Maddi<i class={style.checkIcon + " fa-solid fa-circle-check ps-2"}></i></p>
+                    <div className="mb-3">
+                        <button className={style.jobBtnBlack + " btn rounded-3 me-1"}>Remote</button>
+                        <button className={style.jobBtnBlack + " btn rounded-3 me-1"}>$ 1000 - 1,700</button>
                         <button className={style.jobBtnBlack + " btn rounded-3 me-1 mb-1"}>With Perks</button>
                         <button className={style.jobBtnBlack + " btn rounded-3 me-1"}>Part time</button>
                     </div>
@@ -73,31 +91,13 @@ export default function JobsSlider() {
                     <div class="service-icon d-flex justify-content-center align-items-center m-auto">
                         <i class="fa-solid fa-user-plus fs-4"></i>
                     </div>
-                    <h4 class="pt-4 pb-1">Junior Web Developer</h4>
+                    <h4 class="pt-4 pb-1">Senior Backend Developer</h4>
                     <p>Egypt , Maddi<i class={style.checkIcon + " fa-solid fa-circle-check ps-2"}></i></p>
                     <div className="mb-3">
-                        <button className={style.jobBtnBlack + " btn rounded-3 me-1"}>Ghana</button>
-                        <button className={style.jobBtnBlack + " btn rounded-3 me-1"}>$ 1000 - 1,500</button>
-                        <button className={style.jobBtnBlack + " btn rounded-3 me-1 mb-1"}>With Perks</button>
-                        <button className={style.jobBtnBlack + " btn rounded-3 me-1"}>Part time</button>
-                    </div>
-                    <div>
-                        <button className={style.jobBtnApply + " btn rounded-3 me-1 rounded-3 fs-5"}>Apply<i class="fa-solid fa-link ps-1"></i></button>
-                    </div>
-                </div>
-            </div>
-            <div>
-                <div class={style.jobCard + " text-center bg-white rounded-3 p-4 shadow-sm m-1"}>
-                    <div class="service-icon d-flex justify-content-center align-items-center m-auto">
-                        <i class="fa-solid fa-user-plus fs-4"></i>
-                    </div>
-                    <h4 class="pt-4 pb-1">Junior Web Developer</h4>
-                    <p>Egypt , Maddi<i class={style.checkIcon + " fa-solid fa-circle-check ps-2"}></i></p>
-                    <div className="mb-3">
-                        <button className={style.jobBtnBlack + " btn rounded-3 me-1"}>Ghana</button>
-                        <button className={style.jobBtnBlack + " btn rounded-3 me-1"}>$ 1000 - 1,500</button>
-                        <button className={style.jobBtnBlack + " btn rounded-3 me-1 mb-1"}>With Perks</button>
-                        <button className={style.jobBtnBlack + " btn rounded-3 me-1"}>Part time</button>
+                        <button className={style.jobBtnBlack + " btn rounded-3 me-1"}>OnSite</button>
+                        <button className={style.jobBtnBlack + " btn rounded-3 me-1"}>$ 3000 - 3,700</button>
+                        <button className={style.jobBtnBlack + " btn rounded-3 me-1 mb-1"}>No Perks</button>
+                        <button className={style.jobBtnBlack + " btn rounded-3 me-1"}>Full time</button>
                     </div>
                     <div>
                         <button className={style.jobBtnApply + " btn rounded-3 me-1 rounded-3 fs-5"}>Apply<i class="fa-solid fa-link ps-1"></i></button>
