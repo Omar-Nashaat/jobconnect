260193b56deb5a5e8e0aea0f2a608409b9a5f27a
Omar-Nashaat <omarnashaat18@gmail.com>
2024-04-09 09:31:57 +0200

    Adding Join us and footer

--- src/Components/AnotherHome/AnotherHome.jsx
+++ src/Components/AnotherHome/AnotherHome.jsx
@@ -4,25 +4,83 @@ import JobsSlider from '../JobsSlider/JobsSlider'
 
 export default function AnotherHome() {
     return <>
-        <div className="container">
-            <div className="row">
-                <div className="mt-5">
-                    <div className={style.mainText + " col-md-12 mb-4"}>
-                        <i className="fa-solid fa-dove fa-2xl pe-2 mb-3 mt-4" style={{ color: 'white' }} />
-                        <h1>FindPlus</h1>
-                        <div className={style.yellowLine + " mb-4"}></div>
-                        <div>
-                            <h2 className='fw-light'>Local <span className='fw-bold'>Jobs</span> for the skilled and talented in Africa</h2>
-                            <h6 className='fw-light'><span className='fw-bold'>Powerd</span> by Fleet Labs Ghana</h6>
+
+        {/* start home */}
+        <div id='home'>
+            <div className="container">
+                <div className="row">
+                    <div className="mt-5">
+                        <div className={style.mainText + " col-md-12 mb-4"}>
+                            <i className="fa-solid fa-dove fa-2xl pe-2 mb-3 mt-4" style={{ color: 'white' }} />
+                            <h1>FindPlus</h1>
+                            <div className={style.yellowLine + " mb-4"}></div>
+                            <div>
+                                <h2 className='fw-light'>Local <span className='fw-bold'>Jobs</span> for the skilled and talented in Africa</h2>
+                                <h6 className='fw-light'><span className='fw-bold'>Powerd</span> by Fleet Labs Ghana</h6>
+                            </div>
                         </div>
+                        <JobsSlider />
+                        <button className={style.moreBtn + ' btn d-block m-auto mt-3 d-flex'}>
+                            <div>Show More Jobs</div>
+                        </button>
                     </div>
-                    <JobsSlider />
-                    <button className={style.moreBtn + ' btn d-block m-auto mt-3 d-flex'}>
-                        <div className='pe-2'>Show More Jobs</div>
-                        <i class="fa-solid fa-arrow-down fa-bounce pt-1"></i>
-                    </button>
                 </div>
             </div>
         </div>
+        {/* end home */}
+
+        {/* start join */}
+        <div id='join' className={style.join}>
+            <div className="container w-75">
+                <h1 className='text-white text-center'>Join Us</h1>
+                <div className={style.yellowLine + " mb-4"}></div>
+                <div className="row">
+                    <div className="col-md-6">
+                        <div className={style.findJob}>
+                            <h2>Find a Job</h2>
+                            <p>Finding job opportunity enhances your visibility and captivates potential employers. Such roles stand out amidst the sea of listings, often enjoying extra exposure across various platforms like social media.</p>
+                            <div>
+                                <button className={style.joinBtn + ' btn mt-4'}>Find a job</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-6">
+                        <div className={style.postJob}>
+                            <h2>Post a Job</h2>
+                            <p>Posting a free job listing is budget-friendly and reaches a large pool of job seekers, offering a cost-effective solution for filling positions with qualified candidates. It can also help build a talent pipeline for future job openings.</p>
+                            <div>
+                                <button className={style.postBtn + ' btn mt-4'}>Post a job</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        {/* end join */}
+
+        {/* start footer */}
+        <div id='footer'>
+            <div className="container">
+                <div className={style.upperFooterContent}>
+                    <h5>Hiring for multiple roles?</h5>
+                    <h5>Save time and effort with over +5 job offers , chat with us to enquire more</h5>
+                    <div className={style.whiteLine + " mt-3"}></div>
+                </div>
+                <div className={style.lowerFooterContent}>
+                    <div className="d-flex">
+                        <div className='d-flex align-items-center'>
+                            <i className="fa-solid fa-dove fa-2xl pe-2 mb-3 mt-4" style={{ color: 'white' }} />
+                            <h5 className='mt-3'>FindPlus</h5>
+                        </div>
+                        <p className={style.footerAdderss + ' mt-4'}>Nasr City , 854/12 2024 Nashaat all rights reserved</p>
+                        <p className={style.footerPrivacy + ' mt-4'}>Privacy | Terms of Service</p>
+                        <button className={style.chatBtn + ' btn d-block m-auto mt-3 d-flex'}>
+                            <div>Chat with us</div>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div >
+        {/* end footer */}
     </>
 }
--- src/Components/JobsSlider/JobsSlider.jsx
+++ src/Components/JobsSlider/JobsSlider.jsx
@@ -7,7 +7,7 @@ import style from './JobSlider.module.css';
 export default function JobsSlider() {
     const settings = {
         infinite: true,
-        speed: 1200,
+        speed: 1000,
         slidesToShow: 3,
         slidesToScroll: 1,
         autoplay: true,
@@ -20,9 +20,9 @@ export default function JobsSlider() {
                         <i class="fa-solid fa-user-plus fs-4"></i>
                     </div>
                     <h4 class="pt-4 pb-1">Junior Web Developer</h4>
-                    <p>Egypt , Maddi<i class={style.checkIcon + " fa-solid fa-circle-check ps-2"}></i></p>
+                    <p>Value<i class={style.checkIcon + " fa-solid fa-circle-check ps-2"}></i></p>
                     <div className="mb-3">
-                        <button className={style.jobBtnBlack + " btn rounded-3 me-1"}>Ghana</button>
+                        <button className={style.jobBtnBlack + " btn rounded-3 me-1"}>Maddi</button>
                         <button className={style.jobBtnBlack + " btn rounded-3 me-1"}>$ 1000 - 1,500</button>
                         <button className={style.jobBtnBlack + " btn rounded-3 me-1 mb-1"}>With Perks</button>
                         <button className={style.jobBtnBlack + " btn rounded-3 me-1"}>Part time</button>
@@ -38,9 +38,9 @@ export default function JobsSlider() {
                         <i class="fa-solid fa-user-plus fs-4"></i>
                     </div>
                     <h4 class="pt-4 pb-1">Junior Web Developer</h4>
-                    <p>Egypt , Maddi<i class={style.checkIcon + " fa-solid fa-circle-check ps-2"}></i></p>
+                    <p>Value<i class={style.checkIcon + " fa-solid fa-circle-check ps-2"}></i></p>
                     <div className="mb-3">
-                        <button className={style.jobBtnBlack + " btn rounded-3 me-1"}>Ghana</button>
+                        <button className={style.jobBtnBlack + " btn rounded-3 me-1"}>Maddi</button>
                         <button className={style.jobBtnBlack + " btn rounded-3 me-1"}>$ 1000 - 1,500</button>
                         <button className={style.jobBtnBlack + " btn rounded-3 me-1 mb-1"}>With Perks</button>
                         <button className={style.jobBtnBlack + " btn rounded-3 me-1"}>Part time</button>
@@ -56,9 +56,9 @@ export default function JobsSlider() {
                         <i class="fa-solid fa-user-plus fs-4"></i>
                     </div>
                     <h4 class="pt-4 pb-1">Junior Frontend Developer</h4>
-                    <p>Egypt , Maddi<i class={style.checkIcon + " fa-solid fa-circle-check ps-2"}></i></p>
+                    <p>Value<i class={style.checkIcon + " fa-solid fa-circle-check ps-2"}></i></p>
                     <div className="mb-3">
-                        <button className={style.jobBtnBlack + " btn rounded-3 me-1"}>OnSite</button>
+                        <button className={style.jobBtnBlack + " btn rounded-3 me-1"}>Nasr City</button>
                         <button className={style.jobBtnBlack + " btn rounded-3 me-1"}>$ 2000 - 3,500</button>
                         <button className={style.jobBtnBlack + " btn rounded-3 me-1 mb-1"}>With Perks</button>
                         <button className={style.jobBtnBlack + " btn rounded-3 me-1"}>Full time</button>
@@ -74,7 +74,7 @@ export default function JobsSlider() {
                         <i class="fa-solid fa-user-plus fs-4"></i>
                     </div>
                     <h4 class="pt-4 pb-1">UI/UX Developer</h4>
-                    <p>Egypt , Maddi<i class={style.checkIcon + " fa-solid fa-circle-check ps-2"}></i></p>
+                    <p>Value<i class={style.checkIcon + " fa-solid fa-circle-check ps-2"}></i></p>
                     <div className="mb-3">
                         <button className={style.jobBtnBlack + " btn rounded-3 me-1"}>Remote</button>
                         <button className={style.jobBtnBlack + " btn rounded-3 me-1"}>$ 1000 - 1,700</button>
@@ -92,9 +92,9 @@ export default function JobsSlider() {
                         <i class="fa-solid fa-user-plus fs-4"></i>
                     </div>
                     <h4 class="pt-4 pb-1">Senior Backend Developer</h4>
-                    <p>Egypt , Maddi<i class={style.checkIcon + " fa-solid fa-circle-check ps-2"}></i></p>
+                    <p>Value<i class={style.checkIcon + " fa-solid fa-circle-check ps-2"}></i></p>
                     <div className="mb-3">
-                        <button className={style.jobBtnBlack + " btn rounded-3 me-1"}>OnSite</button>
+                        <button className={style.jobBtnBlack + " btn rounded-3 me-1"}>Nasr City</button>
                         <button className={style.jobBtnBlack + " btn rounded-3 me-1"}>$ 3000 - 3,700</button>
                         <button className={style.jobBtnBlack + " btn rounded-3 me-1 mb-1"}>No Perks</button>
                         <button className={style.jobBtnBlack + " btn rounded-3 me-1"}>Full time</button>
